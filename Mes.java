08492cd1df748a0634947333270e952c023ecdf9
Developerotnotton <42851716+Developerotnotton@users.noreply.github.com>
2018-08-30 20:32:28 -0300

    Add files via upload

--- Mes.java
+++ Mes.java
@@ -12,60 +12,63 @@ public class Mes {
         System.out.println("informe um número para verificar o Mês: ");
         mes = entrada.nextInt();
 
-        switch (mes) {
+        for (;mes < 12; mes++) {
 
-            case 1:
-                System.out.println("janeiro");
-                break;
+            switch (mes) {
 
-            case 2:
-                System.out.println("fevereiro");
-                break;
+                case 1:
+                    System.out.println("janeiro");
+                    break;
 
-            case 3:
-                System.out.println("marco");
-                break;
+                case 2:
+                    System.out.println("fevereiro");
+                    break;
 
-            case 4:
-                System.out.println("abril");
-                break;
+                case 3:
+                    System.out.println("marco");
+                    break;
 
-            case 5:
-                System.out.println("maio");
-                break;
+                case 4:
+                    System.out.println("abril");
+                    break;
 
-            case 6:
-                System.out.println("junho");
-                break;
+                case 5:
+                    System.out.println("maio");
+                    break;
 
-            case 7:
-                System.out.println("julho");
-                break;
+                case 6:
+                    System.out.println("junho");
+                    break;
 
-            case 8:
-                System.out.println("agosto");
-                break;
+                case 7:
+                    System.out.println("julho");
+                    break;
 
-            case 9:
-                System.out.println("setembro");
-                break;
+                case 8:
+                    System.out.println("agosto");
+                    break;
 
-            case 10:
-                System.out.println("outubro");
-                break;
+                case 9:
+                    System.out.println("setembro");
+                    break;
 
-            case 11:
-                System.out.println("novembro");
-                break;
+                case 10:
+                    System.out.println("outubro");
+                    break;
 
-            case 12:
-                System.out.println("dezembro");
-                break;
+                case 11:
+                    System.out.println("novembro");
+                    break;
 
-            default:
-                System.out.println("Mês Inválido");
-                break;
+                case 12:
+                    System.out.println("dezembro");
+                    break;
+
+                default:
+                    System.out.println("Mês Inválido");
+                    break;
+            }
+
         }
-
     }
 }
